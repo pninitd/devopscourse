39f053490ef291c63fabad6e141fbee49fa2d234
pninitd <pninit.dvir@gmail.com>
2021-02-14 19:09:52 +0200

    add my first file

---
+++ lesson7_feb14/lesson7_git.py
@@ -0,0 +1,8 @@
+print(123)
+
+# git init
+# git status
+# git config user.name "pninitd"
+# git config user.email "pninit.dvir@gmail.com"
+# git add lesson7_git.py
+# git commit -m "add my first file"
