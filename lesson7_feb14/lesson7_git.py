c9573b567556b7dd251f9deaae791a71b163b9e5
pninitd <pninit.dvir@gmail.com>
2021-02-14 19:25:41 +0200

    changed

--- lesson7_feb14/lesson7_git.py
+++ lesson7_feb14/lesson7_git.py
@@ -4,5 +4,16 @@ print(123)
 # git status
 # git config user.name "pninitd"
 # git config user.email "pninit.dvir@gmail.com"
-# git add lesson7_git.py
+
+# git add lesson7_feb14/lesson7_git.py
+# git add * (will add you)
+# git add . (will add you)
+
 # git commit -m "add my first file"
+# [master (root-commit) 39f0534] add my first file
+#  1 file changed, 8 insertions(+)
+#  create mode 100644 lesson7_feb14/lesson7_git.py
+
+# git branch feature1
+# git branch
+# git checkout feature1 (gc)
